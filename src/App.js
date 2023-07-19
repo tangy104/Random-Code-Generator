5da9c33d2040e125a0b651e8166672dd6d061857
tangy104 <lemon10swargiary@gmail.com>
2023-07-19 21:26:21 +0530

    added files

--- src/App.js
+++ src/App.js
@@ -1,22 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useState, useEffect} from 'react';
 
 function App() {
+  const [data,setData] = useState({
+    content: 'content will appear here',
+    author: 'author will appear here',
+    tags: ["nice"]
+  });
+
+
+  
+
+  function getQuote(){
+    try{
+      fetch('https://api.quotable.io/random').then(
+      response => response.json()).then(
+        (quote)=>{
+          setData(quote);
+      
+      }
+      )
+    }
+    catch(e){
+      console.log(e);
+    }
+      
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
+        <h2 className='Title'>RANDOM QUOTE GENERATOR</h2>
+        <h3 className='Quote'>QUOTE: {data.content}</h3>
+        <h5 className='Author'>AUTHOR: {data.author}</h5>
+        <h5 className='Tag'>Tag: {data.tags[0]}</h5>
+        <button className='Generator' onClick={getQuote}>Get Quote</button>
       </header>
     </div>
   );
